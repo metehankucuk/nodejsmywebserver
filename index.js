15a38e6db7049e476f8815647403a4f8518f06cc
metehankucuk <email.metehan@gmail.com>
2021-10-13 20:54:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+
+    if (url === '/index') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(`<h2>Index sayfasina hosgeldiniz.</h2>`)
+    }
+    else if (url === '/hakkimda') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(`<h2>Hakkimda sayfasina hosgeldiniz.</h2>`)
+    }
+    else if (url === '/iletisim') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(`<h2>Iletisim sayfasina hosgeldiniz.</h2>`)
+    }
+    else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(`<h2>404 sayfa bulunamadi.</h2>`)
+    }
+    res.end()
+}
+)
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Sunucu port ${port}'de başlatildi.'`)
+});
